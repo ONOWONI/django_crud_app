2df41cec677013972518f0bed1dabbbf5bd4df81
Onowoni <danielonowoni1@gmail.com>
2022-08-01 21:08:48 +0100

    first commit

--- crud_app/crud_app/urls.py
+++ crud_app/crud_app/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django
 from django.contrib import admin
 from django.urls import include, path
 
 
-
 urlpatterns = [
-    path('index/', include('learning.urls')),
-    path('home/', include('learning.urls')),
+    path('', include('learning.urls')),
     path('admin/', admin.site.urls),
 ]
--- crud_app/learning/views.py
+++ crud_app/learning/views.py
@@ -1,11 +1,20 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World, you are my first crud app in django')
+    dests = Destination.objects.first()
+    print(dests)
+    context = { "dests" : dests }
+    return render(request, "home.html", context)
 
 
+# def add(request):
+#     val1 = int(request.POST["num1"])
+#     val2 = int(request.POST["num2"])
+#     res = val1 + val2
+#     return render(request, 'add.html', {'result': res})
 
-def home(request):
-    return HttpResponse('Testing the routing function in django')
