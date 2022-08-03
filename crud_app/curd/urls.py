61e29bd506b382084d5e53b3e54f7f3462be87d5
Onowoni <danielonowoni1@gmail.com>
2022-08-03 22:30:08 +0100

    finished this learning exercise of creating crud functionalities

--- crud_app/curd/urls.py
+++ crud_app/curd/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_post', views.create_post, name='create_post'),
+    path('createpost', views.create_post, name='create_post'),
     path('getpost', views.get_post, name='get_post'),
+    path('getpost/<int:id>', views.get_single_post, name='get_single_post'),
+    path('deletepost/<int:id>', views.delete_post, name='delete_post'),
 ]
--- crud_app/curd/views.py
+++ crud_app/curd/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from learning.models import Post
+from django.contrib import messages
 
 # Create your views here.
 
-
+# post request
 def create_post(request):
     if request.method == 'POST':
         title = request.POST['title']
@@ -12,7 +13,25 @@ def create_post(request):
         post.save()
     return render(request, 'new_post.html')
 
-
+# get request
 def get_post(request):
     post = Post.objects.all()
     return render(request, 'get_post.html', {'posts': post})
+
+# update request
+def get_single_post(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        post.title = title
+        post.content = content
+        post.save()
+    return render(request, 'edit.html', {'posts': post})
+
+# delete request
+def delete_post(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    messages.info(request, "Deleted")
+    return redirect('/')
